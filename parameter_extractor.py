0b4ec8b53cff3de8d30038e764fd5bb441d108a2
Javier Eusamio <jaymaeuro@gmail.com>
2024-10-30 23:50:13 +0100

    - Made some variables in ParameterExtractor class private.

--- parameter_extractor.py
+++ parameter_extractor.py
@@ -4,10 +4,10 @@ from rdkit.Chem import rdMolTransforms
 
 
 class ParameterExtractor:
-    _pdb = None
-    _out = None
-    _atom_list = []
-    _conformer = None
+    __pdb = None
+    __out = None
+    __atom_list = []
+    __conformer = None
     p1 = None
     p2 = None
     r1 = None
@@ -20,13 +20,13 @@ class ParameterExtractor:
 
     def __init__(self, filename: str):
         with open(f"./out/{filename}.out") as f:
-            self._out = f.readlines()
-        self._pdb = Chem.MolFromPDBFile(f"./pdb/{filename}.pdb", removeHs=False)
-        self._conformer = self._pdb.GetConformer()
-        self._atom_list = self._pdb.GetAtoms()
+            self.__out = f.readlines()
+        self.__pdb = Chem.MolFromPDBFile(f"./pdb/{filename}.pdb", removeHs=False)
+        self.__conformer = self.__pdb.GetConformer()
+        self.__atom_list = self.__pdb.GetAtoms()
         self.__set_nbo_prop()
-        self.p1, self.p2 = self._get_phosphorus()
-        self.rh = next(atom for atom in self._atom_list if atom.GetSymbol() == "Rh")
+        self.p1, self.p2 = self.__get_phosphorus()
+        self.rh = next(atom for atom in self.__atom_list if atom.GetSymbol() == "Rh")
         self.bridge = self.__get_bridge()
         self.homo, self.lumo = self.__get_homo_lumo()
         self.dipole = self.__get_dipole()
@@ -40,13 +40,13 @@ class ParameterExtractor:
         blocks = []
         start_line = None
         end_line = None
-        for i, line in enumerate(self._out):
+        for i, line in enumerate(self.__out):
             if re.search(start_pattern, line):
                 start_line = i
             if re.search(end_pattern, line):
                 end_line = i
             if start_line is not None and end_line is not None:
-                blocks.append(self._out[start_line:end_line + include_last_line])
+                blocks.append(self.__out[start_line:end_line + include_last_line])
                 start_line = None
                 end_line = None
         return blocks
@@ -64,13 +64,13 @@ class ParameterExtractor:
         return orbital_blocks
 
     def __get_dipole(self) -> float:
-        dipole = [line for line in self._out if re.search(" X= ", line)][-1]
+        dipole = [line for line in self.__out if re.search(" X= ", line)][-1]
         if str.split(dipole)[-2] == "Tot=":
             return float(str.split(dipole)[-1])
 
     def __set_nbo_prop(self) -> None:
         nbo_blocks = self.__extract_nbo_blocks()
-        for atom in self._atom_list:
+        for atom in self.__atom_list:
             _atom_idx = str(atom.GetIdx() + 1)
             _atom_symbol = atom.GetSymbol()
             for line in nbo_blocks[-1]:
@@ -96,21 +96,21 @@ class ParameterExtractor:
                 break
         return float(homo), float(lumo)
 
-    def _get_phosphorus(self) -> (Chem.Atom, Chem.Atom):
-        p_list = list(filter(lambda x: x.GetSymbol() == "P", self._atom_list))
+    def __get_phosphorus(self) -> (Chem.Atom, Chem.Atom):
+        p_list = list(filter(lambda x: x.GetSymbol() == "P", self.__atom_list))
         return sorted(p_list, key=lambda x: float(x.GetProp("NBO")), reverse=True)
 
     def __get_bridge(self) -> Chem.Atom:
         p1_neighbors = set(map(lambda x: x.GetIdx(), self.p1.GetNeighbors()))
         p2_neighbors = set(map(lambda x: x.GetIdx(), self.p2.GetNeighbors()))
         bridge_idx = list(p1_neighbors.intersection(p2_neighbors))[0]
-        return next(atom for atom in self._atom_list if atom.GetIdx() == bridge_idx)
+        return next(atom for atom in self.__atom_list if atom.GetIdx() == bridge_idx)
 
     def getBondDistance(self, atom_1: Chem.Atom, atom_2: Chem.Atom) -> float:
-        return rdMolTransforms.GetBondLength(self._conformer, atom_1.GetIdx(), atom_2.GetIdx())
+        return rdMolTransforms.GetBondLength(self.__conformer, atom_1.GetIdx(), atom_2.GetIdx())
 
     def getAngle(self, atom_1: Chem.Atom, atom_2: Chem.Atom, atom_3: Chem.Atom) -> float:
-        return rdMolTransforms.GetAngleDeg(self._conformer, atom_1.GetIdx(), atom_2.GetIdx(), atom_3.GetIdx())
+        return rdMolTransforms.GetAngleDeg(self.__conformer, atom_1.GetIdx(), atom_2.GetIdx(), atom_3.GetIdx())
 
     def getTorsionAngle(self, atom_1: Chem.Atom, atom_2: Chem.Atom, atom_3: Chem.Atom, atom_4: Chem.Atom) -> float:
-        return rdMolTransforms.GetDihedralDeg(self._conformer, atom_1.GetIdx(), atom_2.GetIdx(), atom_3.GetIdx(), atom_4.GetIdx())
+        return rdMolTransforms.GetDihedralDeg(self.__conformer, atom_1.GetIdx(), atom_2.GetIdx(), atom_3.GetIdx(), atom_4.GetIdx())
