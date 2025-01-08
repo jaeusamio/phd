57ce479f10034748e6968597cf2d707e82d08de6
Javier <jaymaeuro@gmail.com>
2025-01-08 16:32:04 +0100

    - Added bcolors enum class to add colors to console outputs.

--- parameter_extractor.py
+++ parameter_extractor.py
@@ -545,6 +545,18 @@ class Property(Enum):
     NMR_ANISOTROPIC_NO_RH = "nmr_anisotropic_no_rh"
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class AtomOnPlaneException(Exception):
     pass
 
